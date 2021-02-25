b8b769b00d798c770b812cf2834728d861842e11
koyo-miyamura <nexus9071@gmail.com>
2021-02-25 21:23:00 +0900

    Fix print and Add upload

--- source_deleted_later/lib/to_delete/epson/printer/client.ex
+++ source_deleted_later/lib/to_delete/epson/printer/client.ex
@@ -26,19 +26,26 @@ defmodule Epson.Printer.Client do
 	Prepare job
 	"""
 	def prepare_job( payload, printer_id, %{ "access_token" => access_token } ) do
-		path = Path.join( [ @root, "printers", printer_id, "jobs" ] )
-		header = [
-			"Content-Type": "application/json; charset=utf-8"
-		] |> Keyword.merge( header_oauth2( access_token ) )
+		path   = Path.join( [ @root, "printers", printer_id, "jobs" ] )
+		header = header_content_type() |> Keyword.merge( header_oauth2( access_token ) )
 
 		Json.post( @host, path, Jason.encode!( payload ), header )
 	end
 
 	@doc """
-	Print file
+	Upload file
 	"""
-	def print( %{ "upload_uri" => upload_uri }, file, filename ) do
+	def upload( job = %{ "upload_uri" => upload_uri }, file, filename ) do
 		Json.post_no_response( "#{ upload_uri }&File=#{ filename }", "", file, "Content-Type": "application/octet-stream" )
+		job
+	end
+
+	@doc """
+	Print
+	"""
+	def print( %{ "id" => id }, printer_id ) do
+		path = Path.join( [ @root, "printers", printer_id, "jobs", id, "print" ] )
+		Json.post( @host, path, "", header_content_type() )
 	end
 
 	@doc """
@@ -49,5 +56,6 @@ defmodule Epson.Printer.Client do
 		Json.get( @host, path, header_oauth2( access_token ) )
 	end
 
-	defp header_oauth2(access_token) ,do: [ "Authorization": "Bearer #{access_token}" ]
+	defp header_content_type, do: [ "Content-Type": "application/json; charset=utf-8" ]
+	defp header_oauth2(access_token), do: [ "Authorization": "Bearer #{access_token}" ]
 end
