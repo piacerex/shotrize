87f6f1e370eb099c3e0f6489c8cf530cc11117ad
piacerex <m-mori@digi-dock.com>
2021-02-27 17:54:11 +0900

    - "request_tokens" refactorings  - Rename to build_authorize  - Json.post out to facade, because post is Side-effect, and facade is a place to accommodate  - After, add doctest
    - Removed printer_id from parameters of other functions and passed token instead

--- source_deleted_later/lib/to_delete/epson/printer/client.ex
+++ source_deleted_later/lib/to_delete/epson/printer/client.ex
@@ -1,63 +1,81 @@
 defmodule Epson.Printer.Client do
-	@host "https://api.epsonconnect.com"
-	@root "/api/1/printing"
+  @moduledoc """
+  EpsonConnect API wrapper
 
-	@doc """
-	Request tokens
-	"""
-	def request_tokens( %{ username: username, password: password, basic_credentials: basic_credentials } ) do
-		path = Path.join( [ @root, "oauth2", "auth", "token" ] )
-					 <> "?"
-					 <> URI.encode_query( %{ subject: "printer" } )
-		body = %{
-			"grant_type" => "password",
-			"username"   => username,
-			"password"   => password
-		}
-		header = [
-			"Content-Type":  "application/x-www-form-urlencoded; charset=utf-8",
-			"Authorization": "Basic #{basic_credentials}"
-		]
+  For details, refer to the API document below:
+  https://developer.cp.epson.com/ecapi/downloads/?lang=ja
+  """
 
-		Json.post( @host, path, URI.encode_query( body ), header )
-	end
+  @host "https://api.epsonconnect.com"
+  @root "/api/1/printing"
 
-	@doc """
-	Prepare job
-	"""
-	def prepare_job( payload, printer_id, access_token ) do
-		path   = Path.join( [ @root, "printers", printer_id, "jobs" ] )
+  @doc """
+  Build data for EpsonConnect authorize API
 
-		Json.post( @host, path, Jason.encode!( payload ), header_json() |> Keyword.merge( header_oauth2( access_token ) ) )
-		|> Map.put( "access_token", access_token )
-	end
+  ## Examples
+    iex> Epson.Printer.Client.build_authorize(%{username: "dummy_user", password: "dummy_pw", basic_credentials: "dummy_credencials"})
+    %{"body" => "grant_type=password&password=dummy_pw&username=dummy_user", "header" => ["Content-Type": "application/x-www-form-urlencoded; charset=utf-8", Authorization: "Basic dummy_credencials"], "host" => "https://api.epsonconnect.com", "path" => "/api/1/printing/oauth2/auth/token?subject=printer"}
+  """
+  def build_authorize( %{ username: username, password: password, basic_credentials: basic_credentials } ) do
+    %{
+      "host" => @host, 
+      "path" => Path.join( @root, "/oauth2/auth/token?subject=printer" ), 
+      "body" => %{
+        "grant_type" => "password", 
+        "username"   => username, 
+        "password"   => password, 
+      } |> URI.encode_query, 
+      "header" => [
+        "Content-Type":  "application/x-www-form-urlencoded; charset=utf-8",
+        "Authorization": "Basic #{basic_credentials}"
+      ], 
+    }
+  end
 
-	@doc """
-	Upload file
-	"""
-	def upload( %{ "upload_uri" => upload_uri } = job, file, filename ) do
-		Json.post_raw_response( "#{ upload_uri }&File=#{ filename }", "", file, "Content-Type": "application/octet-stream" )
-		job
-	end
+  @doc """
+  Build data for EpsonConnect job preparer API
 
-	@doc """
-	Print
-	"""
-	def print( %{ "id" => id, "access_token" => access_token } = job, printer_id ) do
-		path = Path.join( [ @root, "printers", printer_id, "jobs", id, "print" ] )
-		Json.post( @host, path, "", header_json() |> Keyword.merge( header_oauth2( access_token ) ) )
-		job
-	end
+  ## Examples
+    iex> Epson.Printer.Client.prepare_job("dummy_body", %{ "subject_id" => "dummy_printer_id", "access_token" => "dummy_access_token" })
+    
+  """
+  def prepare_job( payload, %{ "subject_id" => printer_id, "access_token" => access_token } ) do
+    path = Path.join( @root, "/printers/#{ printer_id }/jobs" )
+    Json.post( @host, path, Jason.encode!( payload ), header_json() |> Keyword.merge( header_oauth2( access_token ) ) )
+  end
 
-	@doc """
-	Get job result
-	"""
-	def get_result( %{ "id" => job_id, "access_token" => access_token } = job, printer_id ) do
-		path = Path.join( [ @root, "printers", printer_id, "jobs", job_id ] )
-		Json.get( @host, path, header_oauth2( access_token ) )
-		job
-	end
+  @doc """
+  Build data for EpsonConnect file uploader API
 
-	defp header_json, do: [ "Content-Type": "application/json; charset=utf-8" ]
-	defp header_oauth2( access_token ), do: [ "Authorization": "Bearer #{ access_token }" ]
+  ## Examples
+  """
+  def upload( %{ "upload_uri" => upload_uri } = job, file, filename ) do
+    Json.post_raw_response( "#{ upload_uri }&File=#{ filename }", "", file, "Content-Type": "application/octet-stream" )
+    job
+  end
+
+  @doc """
+  Build data for EpsonConnect print executor API
+
+  ## Examples
+  """
+  def print( %{ "id" => id } = job, %{ "subject_id" => printer_id, "access_token" => access_token } ) do
+    path = Path.join( [ @root, "printers", printer_id, "jobs", id, "print" ] )
+    Json.post( @host, path, "", header_json() |> Keyword.merge( header_oauth2( access_token ) ) )
+    job
+  end
+
+  @doc """
+  Build data for job result getter API
+
+  ## Examples
+  """
+  def get_result( %{ "id" => job_id } = job, %{ "subject_id" => printer_id, "access_token" => access_token } ) do
+    path = Path.join( [ @root, "printers", printer_id, "jobs", job_id ] )
+    Json.get( @host, path, header_oauth2( access_token ) )
+    job
+  end
+
+  defp header_json, do: [ "Content-Type": "application/json; charset=utf-8" ]
+  defp header_oauth2( access_token ), do: [ "Authorization": "Bearer #{ access_token }" ]
 end
